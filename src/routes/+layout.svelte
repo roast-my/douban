63ebcf02a54899f9501631f9f72dbc43ad49e5e9
anig1scur <anigiscur@gmail.com>
2026-02-19 23:11:04 -0600

    chore: migrate to cloudflare

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
   import '../app.css';
-  import {injectAnalytics} from '@vercel/analytics/sveltekit';
 
   let {children} = $props();
-
-  injectAnalytics();
 </script>
 
 <svelte:head>
